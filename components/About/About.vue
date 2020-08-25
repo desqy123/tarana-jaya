de51385e4dc399713a60944c24eeca75a9ff6a9b
M Desqy <desqy123.desqyyy@gmail.com>
2020-08-26 05:50:30 +0700

    update button on about page (Multiple Downloads)

--- components/About/About.vue
+++ components/About/About.vue
@@ -22,12 +22,44 @@
         </p>
         <button
           class="bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded mt-5"
+          onclick="downloadAll(window.links)"
         >
-          <a
+          Download Company Profile
+
+          <!-- <a
             href="/img/page/about/Tarana Jaya Mandiri Company Profile.pdf"
             download
             >Download Company Profile</a
-          >
+          > -->
+
+          <!-- SCRIPT MULTIPLE DOWNLOADS -->
+          <script>
+            var links = [
+              '/img/page/about/SURAT AKTA NOTARIS ,CV TJM.pdf',
+              '/img/page/about/SURAT IMB ,CV.TJM.pdf',
+              '/img/page/about/SURAT KOMANDITER CV ,TJM.pdf',
+              '/img/page/about/SURAT NPWP ,CV.TJM.pdf',
+              '/img/page/about/SURAT SITU ,CV.TJM.pdf',
+              '/img/page/about/SURAT SIUP ,CV.TJM.pdf',
+              '/img/page/about/Tarana Jaya Mandiri Company Profile.pdf',
+            ]
+
+            function downloadAll(urls) {
+              var link = document.createElement('a')
+
+              link.setAttribute('download', null)
+              link.style.display = 'none'
+
+              document.body.appendChild(link)
+
+              for (var i = 0; i < urls.length; i++) {
+                link.setAttribute('href', urls[i])
+                link.click()
+              }
+
+              document.body.removeChild(link)
+            }
+          </script>
         </button>
       </div>
     </div>
